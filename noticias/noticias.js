64dfbb3be8309ae6f87be0e0c908f69a325f1054
AITNES21 <twitchaitnes@gmail.com>
2025-07-14 01:35:47 +0200

    update

--- noticias/noticias.js
+++ noticias/noticias.js
@@ -17,9 +17,9 @@ class NoticiasManager {
 
     async cargarNoticias() {
         try {
-            const response = await fetch('/noticias.json');
-            if (response.ok) {
-                this.noticias = await response.json();
+            const noticiasLocal = localStorage.getItem('triinfinity_noticias_web');
+            if (noticiasLocal) {
+                this.noticias = JSON.parse(noticiasLocal);
             } else {
                 this.noticias = [];
             }
@@ -268,7 +268,7 @@ class NoticiasManager {
     convertirMarkdownAHTML(markdown) {
         return markdown
             .replace(/^# (.*$)/gm, '<h1>$1</h1>')
-            .replace(/^## (.*$)/gm, '<h2>$2</h2>')
+            .replace(/^## (.*$)/gm, '<h2>$1</h2>')
             .replace(/^### (.*$)/gm, '<h3>$1</h3>')
             .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
             .replace(/\*(.*?)\*/g, '<em>$1</em>')
